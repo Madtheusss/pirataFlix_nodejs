fefd184230e3ec9919c6c9720282662342fc9a55
Matheus <madtheusss@hotmail.com>
2022-04-07 20:38:24 -0300

    att

---
+++ pirataflix/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from './Navigation.module.css';
+
+const Navigation = () => {
+    return(
+        <div className={styles.navigation}>
+            Nav
+        </div>
+    )
+}
+
+export default Navigation;
