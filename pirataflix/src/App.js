2f73189faae840f214f13edf9e1705c9efb2b981
Matheus <madtheusss@hotmail.com>
2022-04-08 09:19:13 -0300

    2605

--- pirataflix/src/App.js
+++ pirataflix/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import './normalize.css';
+import './index.css'
 import Navigation from "./components/Navigation/Navigation";
+import Billboard from "./components/Billboard/Billboard";
+import Titles from "./components/Titles/Titles";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navigation /> 
+      <Billboard />
+      <Titles />
+      <Footer />
     </div>
   );
 }
--- pirataflix/src/components/Navigation/Navigation.jsx
+++ pirataflix/src/components/Navigation/Navigation.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
+import logo from './assets/logo.png';
 import styles from './Navigation.module.css';
 
 const Navigation = () => {
+    const navItems = [
+        'Inicio',
+        'Séries',
+        'Filmes',
+        'Mais recentes',
+        'Minha lista'
+    ]
     return(
         <div className={styles.navigation}>
-            Nav
+            <img src={logo} alt="LogoNetflix" />
+            <ul className={styles.navigationList}>
+                {navItems.map((item) => (
+                    <li className={styles.navigationItem} key={item}>{item}</li>
+                ))}
+            </ul>
         </div>
     )
 }
